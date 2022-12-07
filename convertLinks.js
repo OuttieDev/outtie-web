429581cea83a9e1273325ef224305bb28721d9ce
krishmoran <43583683+krishmoran@users.noreply.github.com>
2022-12-06 19:46:23 -0500

    ConvertLinks and ConvertAllLinks
    Added capability to convert one link at a time, or run on all links at once. Will add to documentation how to initialize

---
+++ convertLinks.js
@@ -0,0 +1,46 @@
+const dotenv = require('dotenv');
+
+//load environment variables
+dotenv.config();
+
+//convert single link to outtie api call
+export function convertLink(link) {
+    //get href attribute of link
+    let href = link.getAttribute("href");
+
+    //convert href to link with API call
+    let convertedLink = "https://api.outtie.io/create_link?sdkKey=" + process.env.SDK_KEY + "&clientId=" + process.env.CLIENT_ID + "&redirect=" + href;
+
+    //replace href with api call
+    link.setAttribute("href", convertedLink);
+}
+
+
+//function that loops through href tags and converts them to proper outtie api calls
+export function convertAllLinks() {
+    //get list of all links on page
+    let links = document.getElementsByTagName("a");
+
+    //loop through links in foreach loop and convert them to api calls
+    links.array.forEach(element => {
+        //get href attribute of link
+        let href = element.getAttribute("href");
+
+
+        //convert href to link with API call
+        let convertedLink = "https://api.outtie.io/create_link?sdkKey=" + process.env.SDK_KEY + "&clientId=" + process.env.CLIENT_ID + "&redirect=" + href;
+
+        //replace href with api call
+        element.setAttribute("href", convertedLink);
+
+    }); {
+    }
+
+
+}
+
+// example link
+// Example: https://api.outtie.io/create_link?sdkKey=ljLuuEFFWVyOmP8ThCHsFQ&clientId=oOT55l7kNvEeZDvVLPkIIQ&redirect=https://www.aloyoga.com”
+
+//call function when page loads, if running standalone to convert all 
+// window.onload = function () { convertAllLinks();}
