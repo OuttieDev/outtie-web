898ce6776bb8f9e71f705891eee1fbc46a43e06b
krishmoran <43583683+krishmoran@users.noreply.github.com>
2022-12-29 21:02:53 -0500

    updated export statements

--- outtie.js
+++ outtie.js
@@ -1,5 +1,3 @@
-import getModuleLocation from "debundle/src/utils/getModuleLocation";
-
 //convert single link to outtie api call
 export function convertLink(link, clientId, sdkKey) {
     //get href attribute of link
@@ -14,27 +12,32 @@ export function convertLink(link, clientId, sdkKey) {
 
 
 //function that loops through href tags and converts them to proper outtie api calls
-function convertAllLinks(sdkKey, clientId) {
-    //get list of all links on page
+export function convertAllLinks(sdkKey, clientId) {
+    // get list of all links on page
     let links = document.getElementsByTagName("a");
-
-    //loop through links in foreach loop and convert them to api calls
-    links.array.forEach(element => {
-        //get href attribute of link
-        let href = element.getAttribute("href");
-
-        if (href) {
-            if (href.includes("http") || href.includes("https")) {
-                //convert href to link with API call
-                let convertedLink = "https://api.outtie.io/create_link?sdkKey=" + sdkKey + "&clientId=" + clientId + "&redirect=" + href;
-
-                //replace href with api call
-                element.setAttribute("href", convertedLink);
+  
+    // loop through links in foreach loop and convert them to api calls
+    for (let element of links) {
+      // get href attribute of link
+      let href = element.getAttribute("href");
+  
+      if (href) {
+        if (href.includes("http") || href.includes("https")) {
+          // convert href to link with API call
+          let convertedLink =
+            "https://api.outtie.io/create_link?sdkKey=" +
+            sdkKey +
+            "&clientId=" +
+            clientId +
+            "&redirect=" +
+            href;
+  
+          // replace href with api call
+          element.setAttribute("href", convertedLink);
         }
-    };
- }); {
+      }
     }
-}
+  }
 
 // Example: https://api.outtie.io/create_link?sdkKey={sdkKey}&clientId={clientId}&redirect=https://www.aloyoga.com
 
@@ -42,5 +45,4 @@ export function initializeOuttie(sdkKey, clientId) {
     window.onload = function () { convertAllLinks(sdkKey, clientId); }
 }
 
-//export function to convert all links on page
-export default initializeOuttie;
+//TODO: Figure out why export is having issues within chrome extension
